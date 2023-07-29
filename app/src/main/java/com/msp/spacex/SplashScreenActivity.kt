b0e4a9a04ee5e7379613c68cdece752472c7d89c
Youssef <youssef@peoplin.com>
2023-07-29 03:22:32 +0200

    Init Project

---
+++ app/src/main/java/com/msp/spacex/SplashScreenActivity.kt
@@ -0,0 +1,13 @@
+package com.msp.spacex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.coroutines.delay
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+    }
+}
